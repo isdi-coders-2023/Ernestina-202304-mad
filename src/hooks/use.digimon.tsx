869d640476c7cd89705834608fd7c8ec78d8d7af
Raquel Adrados <radrados00@gmail.com>
2023-05-31 13:34:22 +0200

    Include reduce in the hook

--- src/hooks/use.digimon.tsx
+++ src/hooks/use.digimon.tsx
@@ -1,37 +1,24 @@
 import { useCallback, useEffect, useMemo, useReducer } from "react";
-import { Digimon } from "../models/digimon";
 import { ApiRepository } from "../services/api.repository";
 import { DigimonState, digimonReducer } from "../reducers/reducer";
 import * as ac from "../reducers/actions.creator";
 
 export function useDigimon() {
-
   const initialState: DigimonState = {
     digimons: [],
   };
-  const querySize = "?pageSize=20";
-  // const [digimon, setDigimon] = useState<Digimon[]>([]);
 
   const [DigimonState, dispatch] = useReducer(digimonReducer, initialState);
 
-  const query = "?pageSize=20&page=";
+  const querySize = "?pageSize=20&page=";
   const currentPage = 3;
-  const [digimon, setDigimon] = useState<Digimon[]>([]);
-
 
   const repo: ApiRepository = useMemo(() => new ApiRepository(), []);
 
   const handleLoad = useCallback(async () => {
+    const loadedDigimon = await repo.getAll(querySize, currentPage);
 
-    const loadedDigimon = await repo.getAll(querySize);
-    //const content = loadedDigimon.content;
-    //setDigimon(content);
-    dispatch(ac.loadDigimonAction(loadedDigimon));
-
-    const loadedDigimon = await repo.getAll(query, currentPage);
-    const pero = loadedDigimon.content;
-    setDigimon(pero);
-
+    dispatch(ac.loadDigimonAction(loadedDigimon.content));
   }, [repo]);
 
   useEffect(() => {
