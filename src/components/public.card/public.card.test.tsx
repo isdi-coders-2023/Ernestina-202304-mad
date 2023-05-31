0f4eeae34cf365a651dfa148ae783fc9c951a05d
Raquel Adrados <radrados00@gmail.com>
2023-05-31 10:22:56 +0200

    Add test file publiccard

---
+++ src/components/public.card/public.card.test.tsx
@@ -0,0 +1,8 @@
+import { PublicCard } from "./public.card";
+import { render } from "@testing-library/react";
+import "@testing-library/jest-dom";
+describe("Given the component PublicCard", () => {
+  describe("When it is instantiated", () => {
+    test("Then it should should be in the document", () => {});
+  });
+});
