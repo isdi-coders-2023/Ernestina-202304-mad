533ff576164f02eb6c38c8cab21f94d3df3a2e27
Raquel Adrados <radrados00@gmail.com>
2023-05-31 11:54:43 +0200

    Add publiccard test

--- src/components/public.card/public.card.test.tsx
+++ src/components/public.card/public.card.test.tsx
@@ -1,8 +1,22 @@
 import { PublicCard } from "./public.card";
-import { render } from "@testing-library/react";
+import { screen, render } from "@testing-library/react";
 import "@testing-library/jest-dom";
+
 describe("Given the component PublicCard", () => {
   describe("When it is instantiated", () => {
-    test("Then it should should be in the document", () => {});
+    render(
+      <PublicCard
+        item={{
+          id: 0,
+          name: "",
+          url: "",
+          image: "",
+        }}
+      ></PublicCard>
+    );
+    test("Then it should should be in the document", () => {
+      const element = screen.getByRole("listitem");
+      expect(element).toBeInTheDocument();
+    });
   });
 });
