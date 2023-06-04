05a9a16a41a59848e5585b6176b9a1a5330306f7
Sergio Mata <sergiomataherrero@gmail.com>
2023-06-04 13:58:10 +0200

    Add response not ok to getAll api method

--- src/services/api.repository.test.ts
+++ src/services/api.repository.test.ts
@@ -2,16 +2,31 @@ import { DigimonLink } from "../models/digimon";
 import { ApiRepository } from "./api.repository";
 
 describe("Given the class ApiRepository", () => {
-  const repo = new ApiRepository();
-  const mockDigimon = [{ name: "patata", id: 2 }] as unknown as DigimonLink;
-  global.fetch = jest.fn().mockResolvedValue({
-    json: () => Promise.resolve(mockDigimon),
-    ok: true,
-  });
-  describe("When it is instantiated implements DigimonRepo", () => {
-    test("Then it should returns", async () => {
-      const get = await repo.getAll("raton", 1);
+  describe("When it is instantiated implements DigimonRepo and the response is ok", () => {
+    test("Then it should return", async () => {
+      const repo = new ApiRepository();
+      const mockDigimon = [{ name: "patata", id: 2 }] as unknown as DigimonLink;
+      global.fetch = jest.fn().mockResolvedValue({
+        json: () => Promise.resolve(mockDigimon),
+        ok: true,
+      });
+
+      const get = await repo.getAll("patata", 1);
       expect(get).toEqual(mockDigimon);
+    });
+  });
+
+  describe("When it is instantiated and the response is not ok", () => {
+    test("Then it should return", async () => {
+      const mockError = "Error";
+      global.fetch = jest.fn().mockResolvedValue({
+        ok: false,
+        status: 400,
+        statusText: "Error",
+      });
+      const repo = await new ApiRepository();
+
+      expect(repo.getAll("test", 1)).rejects.toThrow(mockError);
     });
   });
 });
